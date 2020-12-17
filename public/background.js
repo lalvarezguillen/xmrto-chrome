d7d47dee602074938adc67bcbbbb2efd8b99d545
Alexander Taranov <sander.taranov@gmail.com>
2020-12-17 17:57:45 +0100

    Updated manifest.json

--- public/background.js
+++ public/background.js
@@ -1,7 +1,4 @@
 window.chrome.runtime.onMessage.addListener(({ type, options, callback = () => {} }) => {
-  if (type === "notification") {
-    chrome.notifications.create('', options, callback);
-  }
   if (type === "options") {
     if (chrome.runtime.openOptionsPage) {
       chrome.runtime.openOptionsPage();
